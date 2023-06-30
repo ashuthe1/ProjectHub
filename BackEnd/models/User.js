ccbd7584dcda09eceb357625ec48b6056bf85bac
ashuthe1 <ashutosh251848@gmail.com>
2023-07-01 02:04:16 +0530

    Created User Schema for Storing User Info

---
+++ BackEnd/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName:{
+        type: String,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    profilePicUrl:{
+        type: String,
+        default: "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=740&t=st=1687432999~exp=1687433599~hmac=33e172819c9cbfbc3af7d6e1c2b39726359c81c79b4f4fb6dc9b0fbc40f81cc9",
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = { User };
