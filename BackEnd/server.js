7928086a16f83f308fde51fa5b23fe87851b85dc
ashuthe1 <ashutosh251848@gmail.com>
2023-07-01 01:56:33 +0530

    Created the Start Server of Project

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 4000;
-app.use(expree.json());
+app.use(express.json());
 
-app.listen()
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("DataBase is connected"))
+  .catch((err) =>{
+    console.log(err)
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
