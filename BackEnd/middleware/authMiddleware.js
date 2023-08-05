d7c69aecad36125e2aa9c5a23f8b286d9271d471
ashuthe1 <ashutosh251848@gmail.com>
2023-08-05 15:03:53 +0530

    Added verify token middleware

---
+++ BackEnd/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const SECRET_ACCESS_TOKEN = process.env.SECRET_ACCESS_TOKEN || 'OURHARDCODEDSECRET';
+
+const authenicateToken = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, SECRET_ACCESS_TOKEN, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = { authenicateToken };
