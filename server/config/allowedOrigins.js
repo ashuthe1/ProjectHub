c7693ba35205df38232b45d48db186427483c443
ashuthe1 <ashutosh251848@gmail.com>
2024-02-14 15:17:32 +0530

    Cors: Allowed Origins Updated

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
     "http://127.0.0.1:5173",
-    "http://localhost:5173",
+    "http://0.0.0.0:5173",
+    "http://localhost:5173", // development
+    "https://theprojecthub.vercel.app/" // production
 ];
   
 module.exports = allowedOrigins;
