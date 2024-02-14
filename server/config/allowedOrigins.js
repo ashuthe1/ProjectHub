130b08daf47dd124642000c75b4c0111a1933f65
ashuthe1 <ashutosh251848@gmail.com>
2024-02-14 15:41:45 +0530

    Fix: cors errors

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     "http://127.0.0.1:5173",
     "http://0.0.0.0:5173",
     "http://localhost:5173", // development
-    "https://theprojecthub.vercel.app/" // production
+    "https://theprojecthub.vercel.app/", // production
+    "https://theprojecthubserver.onrender.com"
 ];
   
 module.exports = allowedOrigins;
