f2ce287fa34fe699d1df8a76a5eb8fc0a875f15f
ashuthe1 <ashutosh251848@gmail.com>
2024-02-27 22:39:32 +0530

    Add: New Domain

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,8 +2,9 @@ const allowedOrigins = [
     "http://127.0.0.1:5173",
     "http://0.0.0.0:5173",
     "http://localhost:5173", // development
-    "https://theprojecthub.vercel.app", // production
+    "https://theprojecthub.vercel.app", // vercel production
     "https://theprojecthubserver.onrender.com",
+    "https://projecthub.gautamashutosh.com", // production
     "https://pink-energetic-caridea.cyclic.app" // cyclic deployment
 ];
   
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -14,6 +14,7 @@ const userSchema = new mongoose.Schema(
     },
     githubProfile: {
       type: String,
+      default: ""
     },
     followersData: {
       count: {
