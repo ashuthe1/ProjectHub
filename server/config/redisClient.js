37549630649e0f7c560b5c7b55498b140fcc446d
ashuthe1 <ashutosh251848@gmail.com>
2024-06-02 15:23:46 +0530

    Updated to local redis-server

--- server/config/redisClient.js
+++ server/config/redisClient.js
@@ -1,16 +1,11 @@
 const { createClient } = require('redis');
 
-const redisClient = createClient({
-    password: process.env.REDIS_SERVER_PASSWORD,
-    socket: {
-        host: process.env.REDIS_SERVER_URL,
-        port: process.env.REDIS_SERVER_PORT
-    }
-});
+const redisClient = createClient({});
 
 async function connectRedisClient() {
     await redisClient.connect();
 };
+
 connectRedisClient();
 
 module.exports = redisClient;
--- websocket/config/redisClient.js
+++ websocket/config/redisClient.js
@@ -1,12 +1,6 @@
 const { createClient } = require('redis');
 
-const redisClient = createClient({
-    password: process.env.REDIS_SERVER_PASSWORD,
-    socket: {
-        host: process.env.REDIS_SERVER_URL,
-        port: process.env.REDIS_SERVER_PORT
-    }
-});
+const redisClient = createClient({});
 
 async function connectRedisClient() {
     await redisClient.connect();
